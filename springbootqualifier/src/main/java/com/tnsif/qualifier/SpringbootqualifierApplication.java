1ae84ecfc5aa526064ddb99d7f3abb2679845610
Kaviarasan79 <kaviarasan0709@gmail.com>
2025-09-08 22:45:22 +0530

    commit

---
+++ springbootqualifier/src/main/java/com/tnsif/qualifier/SpringbootqualifierApplication.java
@@ -0,0 +1,13 @@
+package com.tnsif.qualifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootqualifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootqualifierApplication.class, args);
+	}
+
+}
